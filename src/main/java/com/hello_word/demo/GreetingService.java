75915757ef307de890b738eee2cebee7043f82bf
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-08-26 18:22:48 -0500

    Declar  una varible como static

--- src/main/java/com/hello_word/demo/GreetingService.java
+++ src/main/java/com/hello_word/demo/GreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class GreetingService {
 
-	private final String MENSAJE = "Hola Mundo"; 
+	private static final String MENSAJE = "Hola Mundo"; 
 
 
 	public String helloWord(){
